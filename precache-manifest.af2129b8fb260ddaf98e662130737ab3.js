c65a95d12869f2dc6cdbe6bd3a6a13b363f01191
Alicia Liu <156869760+aliciaxl@users.noreply.github.com>
2025-02-27 17:55:55 -0800

    Updates

---
+++ precache-manifest.af2129b8fb260ddaf98e662130737ab3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7202b768144adbdf6e12375b20f3848",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "7870a23a848bb639496d",
+    "url": "/quiz-app/static/css/main.7ea19e7a.chunk.css"
+  },
+  {
+    "revision": "874eea42154ea1f56df5",
+    "url": "/quiz-app/static/js/2.d84c9f37.chunk.js"
+  },
+  {
+    "revision": "7870a23a848bb639496d",
+    "url": "/quiz-app/static/js/main.bd4dc05c.chunk.js"
+  },
+  {
+    "revision": "929021f0506d4ac5f2ec",
+    "url": "/quiz-app/static/js/runtime~main.9947e4ab.js"
+  }
+]);
